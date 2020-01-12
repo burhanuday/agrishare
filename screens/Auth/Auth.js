fcf8a7556009043993036c474e7d4b034f9b011c
Burhanuday <udaipurwalaburhanuddin@gmail.com>
2020-01-12 10:13:10 +0530

    work on auth screen

--- screens/Auth/Auth.js
+++ screens/Auth/Auth.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { Block, Text } from "../../components/index";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Block, Text, Input, Button } from "../../components/index";
 
 const Auth = props => {
+  const [phone, setPhone] = useState("");
+
   return (
-    <Block>
-      <Text>Auth</Text>
+    <Block padding={20} safe middle>
+      <Block middle flex={0.1}>
+        <Text>Phone:</Text>
+        <Input
+          marginTop={15}
+          onChangeText={val => setPhone(val)}
+          value={phone}
+          placeholder="Enter phone number"
+          textContentType="telephoneNumber"
+          autoCompleteType="tel"
+          keyboardType="phone-pad"
+        />
+        <Block center marginTop={10}>
+          <Button
+            onPress={() => {
+              props.navigation.navigate("Authenticated");
+            }}
+            style={styles.loginButton}
+            primary
+          >
+            <Block center middle>
+              <Text white bold>
+                LOGIN
+              </Text>
+            </Block>
+          </Button>
+        </Block>
+      </Block>
     </Block>
   );
+};
+
+const styles = StyleSheet.create({
+  loginButton: {
+    width: 100
+  }
+});
+
+Auth.navigationOptions = {
+  headerShown: false
 };
 
 export default Auth;
