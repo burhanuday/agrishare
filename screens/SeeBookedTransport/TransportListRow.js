a0a77a369b51d7acb78b25449479c3f3d4628b6a
Burhanuday <udaipurwalaburhanuddin@gmail.com>
2020-01-16 20:56:50 +0530

    showing total duration and distance

--- screens/SeeBookedTransport/TransportListRow.js
+++ screens/SeeBookedTransport/TransportListRow.js
@@ -3,6 +3,7 @@ import { StyleSheet, Linking } from "react-native";
 import { Block, Text, Card, Button, COLORS } from "../../components";
 import Axios from "axios";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import moment from "moment";
 
 const TransportListRow = props => {
   const [startAddress, setStartAddress] = useState("");
@@ -10,6 +11,40 @@ const TransportListRow = props => {
   const [waypoints, setWaypoints] = useState([]);
   const [driver, setDriver] = useState({});
   const [vehicle, setVehicle] = useState({});
+  const [departure, setDeparture] = useState("");
+  const [duration, setDuration] = useState({
+    totalDistance: 0,
+    totalDuration: 0
+  });
+
+  const getDuration = async (startAddress, endAddress, waypoints) => {
+    const waypointsString = waypoints.reduce((total, current) => {
+      return `${total}${current.latitude},${current.longitude}|`;
+    }, "");
+    console.tron.log("e req");
+
+    //console.tron.log("way string", waypointsString);
+    const result = await Axios.get(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${startAddress.geometry.lat},${startAddress.geometry.lng}&destination=${endAddress.geometry.lat},${endAddress.geometry.lng}&waypoints=${waypointsString}&key=AIzaSyB_6Gc31BMUDvuSEMz8AYWjTbza4UvytmQ`
+    );
+    console.tron.log("results a sa s", result);
+
+    let totalDistance = 0;
+    let totalDuration = 0;
+    console.tron.log("be legs");
+
+    const legs = result.data.routes[0].legs;
+    console.tron.log(result);
+
+    console.tron.log("legs", legs);
+
+    for (var i = 0; i < legs.length; ++i) {
+      totalDistance += legs[i].distance.value;
+      totalDuration += legs[i].duration.value;
+    }
+    console.tron.log("duration", totalDistance, totalDuration);
+    return { totalDistance, totalDuration };
+  };
 
   const fetchData = async () => {
     console.tron.log("req", props.request);
@@ -20,6 +55,7 @@ const TransportListRow = props => {
     const waypoints = journey.waypoints;
     const driver = journey.posted_by;
     const vehicle = journey.vehicle;
+    const departure = journey.departure;
 
     try {
       const responseStart = await Axios.get(
@@ -31,15 +67,26 @@ const TransportListRow = props => {
       const responseEnd = await Axios.get(
         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${end.lat},${end.lng}&key=AIzaSyB_6Gc31BMUDvuSEMz8AYWjTbza4UvytmQ`
       );
+      console.tron.log("dur in main fn", duration);
+
       const endAddress = responseEnd.data.results[0].formatted_address;
       //console.tron.log("result", endAddress.formatted_address);
+      const duration = await getDuration(
+        { address: startAddress, geometry: start },
+        { address: endAddress, geometry: end },
+        waypoints
+      );
 
       setStartAddress({ address: startAddress, geometry: start });
       setEndAddress({ address: endAddress, geometry: end });
       setWaypoints(waypoints);
       setDriver(driver);
       setVehicle(vehicle);
+      setDeparture(departure);
+      setDuration(duration);
     } catch (error) {
+      console.tron.log("error", error);
+
       alert("There was an error with the request");
     }
     return "done";
@@ -54,7 +101,7 @@ const TransportListRow = props => {
     //console.tron.log("way string", waypointsString);
 
     Linking.openURL(
-      `https://www.google.com/maps/dir/?api=1&origin=${startAddress.geometry.lat},${startAddress.geometry.lng}&destination=${endAddress.geometry.lat},${endAddress.geometry.lng}&waypoints=${waypointsString}`
+      `https://www.google.com/maps/dir/?api=1&origin=${startAddress.geometry.lat},${startAddress.geometry.lng}&destination=${endAddress.geometry.lat},${endAddress.geometry.lng}&waypoints=optimize:true|${waypointsString}`
     );
   };
 
@@ -62,20 +109,34 @@ const TransportListRow = props => {
     fetchData().then(res => console.log(res));
   }, [props]);
 
+  const getHoursFromSeconds = seconds => {
+    let hours = seconds / 3600;
+    hours = String(hours);
+    hours = hours.substring(0, 4);
+    return hours + " hrs";
+  };
+
+  const getKmsFromM = meters => {
+    let distance = meters / 1000;
+    distance = String(distance);
+    distance = distance.substring(0, 5);
+    return distance + " kms";
+  };
+
   return (
     <Card margin={5} outlined>
       <Block style={styles.container} padding={5}>
         <Block row marginBottom={10}>
           <Block paddingLeft={5} paddingRight={5} flex={1}>
             <Text primary small>
-              NAME
+              DRIVER NAME
             </Text>
             <Text>{driver.name}</Text>
           </Block>
 
           <Block paddingLeft={5} paddingRight={5} flex={1}>
             <Text primary small>
-              PHONE
+              DRIVER PHONE
             </Text>
             <Text>{driver.phone}</Text>
           </Block>
@@ -84,16 +145,43 @@ const TransportListRow = props => {
         <Block row marginBottom={10}>
           <Block paddingLeft={5} paddingRight={5} flex={1}>
             <Text primary small>
-              MODEL
+              TRUCK MAKE
             </Text>
             <Text>{vehicle.model}</Text>
           </Block>
 
           <Block paddingLeft={5} paddingRight={5} flex={1}>
             <Text primary small>
-              PLATE
+              LICENSE PLATE
             </Text>
             <Text>{vehicle.license_plate}</Text>
+          </Block>
+        </Block>
+
+        <Block row marginBottom={10}>
+          <Block paddingLeft={5} paddingRight={5} flex={1}>
+            <Text primary small>
+              DEPARTURE
+            </Text>
+            <Text>
+              {moment(departure, "YYYY-MM-DDTHH:mm:ss.SSSZ").format(
+                "DD-MM-YYYY"
+              )}
+            </Text>
+          </Block>
+
+          <Block paddingLeft={5} paddingRight={5} flex={1}>
+            <Text primary small>
+              DISTANCE
+            </Text>
+            <Text>{getKmsFromM(duration.totalDistance)}</Text>
+          </Block>
+
+          <Block paddingLeft={5} paddingRight={5} flex={1}>
+            <Text primary small>
+              DURATION
+            </Text>
+            <Text>{getHoursFromSeconds(duration.totalDuration)}</Text>
           </Block>
         </Block>
 
